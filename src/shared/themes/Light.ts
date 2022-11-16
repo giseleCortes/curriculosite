b5194fba9dc56f5c46fc3efb31b4ff78fecacae1
giseleCortes <86819936+giseleCortes@users.noreply.github.com>
2022-11-15 23:52:55 -0300

    tema claro

---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,24 @@
+import { createTheme} from '@mui/material';
+import { cyan, deepPurple } from '@mui/material/colors';
+
+export const LightTheme = createTheme ({
+    palette:{
+        primary:{
+            main: deepPurple[500],
+            dark:deepPurple[600],
+            light: deepPurple[300],
+            contrastText:'#f5f5f5',
+        },
+
+        secondary:{
+            main: cyan[500],
+            dark:cyan[400],
+            light: cyan[300],
+            contrastText:'#f5f5f5',
+        },
+        background:{
+            default:"#ede7f6",
+            paper: '#ffffff',
+        }
+    }    
+});
