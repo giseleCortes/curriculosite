83f9bffe896b714ed0522bdca9872f1d84832a9c
giseleCortes <86819936+giseleCortes@users.noreply.github.com>
2022-11-16 02:40:02 -0300

    drawer 2

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -7,7 +7,9 @@ export const MenuLateral: React.FC <{ children: React.ReactNode }> = ({ children
        <>
         <Drawer open={true} variant = 'permanent'>
             <Box width={theme.spacing(28)}>
-            <Avatar alt="Gisele Cortes" src="" />   
+                <Box width="100%" height={theme.spacing(20)} display="flex">
+                    <Avatar alt="Gisele Cortes" src="https://media-exp1.licdn.com/dms/image/C4E03AQFqXWiPOiAwKw/profile-displayphoto-shrink_800_800/0/1657586288390?e=1674086400&v=beta&t=X6o01AK7H_C-MX5j85zE7zueA1SNWMi4M3rq99548uA" /> 
+                </Box>
             </Box>
         </Drawer>
 
