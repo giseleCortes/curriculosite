28166d1617114248a5d7fb98597518f2509e0afb
giseleCortes <86819936+giseleCortes@users.noreply.github.com>
2022-11-16 03:49:15 -0300

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 
-import { BrowserRouter } from "react-router-dom";
-import { AppRoutes } from "./routes";
-import { MenuLateral } from "./shared/components";
-import { AppThemeProvider } from "./shared/contexts";
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
+import { MenuLateral } from './shared/components';
+import { AppThemeProvider } from './shared/contexts';
 
 
 export const App = () => {
-  return (
-    <AppThemeProvider>
-      <BrowserRouter>
-        <MenuLateral>
-          <AppRoutes />
-        </MenuLateral>
+	return (
+		<AppThemeProvider>
+			<BrowserRouter>
+				<MenuLateral>
+					<AppRoutes />
+				</MenuLateral>
       
-      </BrowserRouter>
-    </AppThemeProvider>
-  );
-}
+			</BrowserRouter>
+		</AppThemeProvider>
+	);
+};
 
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,14 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAppThemeContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
-    const {toggleTheme} = useAppThemeContext();
+	const {toggleTheme} = useAppThemeContext();
 
-    return(
-        <Routes>
-            <Route path="/pagina-inicial" element ={<Button variant='contained' color='primary' onClick={toggleTheme}> Teste</Button>} />
+	return(
+		<Routes>
+			<Route path="/pagina-inicial" element ={<Button variant='contained' color='primary' onClick={toggleTheme}> Teste</Button>} />
 
-            <Route path="*" element ={<Navigate to="/pagina-inicial" />} />
-        </Routes>
+			<Route path="*" element ={<Navigate to="/pagina-inicial" />} />
+		</Routes>
 
-    );
-}
+	);
+};
