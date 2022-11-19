4189b6a1f5734344a11c269a3b0575f0ad0b7d2c
giseleCortes <86819936+giseleCortes@users.noreply.github.com>
2022-11-19 16:33:30 -0300

    responsividade menu lateral

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
 import { MenuLateral } from './shared/components';
-import { AppThemeProvider } from './shared/contexts';
+import { AppThemeProvider, DrawerProvider } from './shared/contexts';
 
 
 export const App = () => {
 	return (
 		<AppThemeProvider>
-			<BrowserRouter>
-				<MenuLateral>
-					<AppRoutes />
-				</MenuLateral>
+			<DrawerProvider>
+				<BrowserRouter>
+
+					<MenuLateral>
+						<AppRoutes />
+					</MenuLateral>
       
-			</BrowserRouter>
+				</BrowserRouter>
+			</DrawerProvider>
 		</AppThemeProvider>
 	);
 };
--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,11 +1,15 @@
-import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from '@mui/material';
+import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
+import { useDrawerContext } from '../../contexts';
 
 
 export const MenuLateral: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 	const theme = useTheme();
+	const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+	const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
 	return (
 		<>
-			<Drawer open={true} variant='permanent'>
+			<Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose= {toggleDrawerOpen}>
 				<Box width={theme.spacing(28)} height='100%' display='flex' flexDirection='column'>
 					<Box width="100%" height={theme.spacing(20)} display="flex" alignItems='center' justifyContent='center'>
 						<Avatar sx={{ height: theme.spacing(18), width: theme.spacing(18) }}
@@ -16,7 +20,7 @@ export const MenuLateral: React.FC<{ children: React.ReactNode }> = ({ children 
 						<List component='nav'>
 							<ListItemButton>
 								<ListItemIcon>
-									<Icon>mood</Icon>
+									<Icon>autostories</Icon>
 								</ListItemIcon>
 								<ListItemText primary="Sobre" />
 							</ListItemButton>
@@ -27,7 +31,7 @@ export const MenuLateral: React.FC<{ children: React.ReactNode }> = ({ children 
 				</Box>
 			</Drawer>
 
-			<Box height='100vh' marginLeft={theme.spacing(28)}>
+			<Box height='100vh' marginLeft={smDown ? 0 : theme.spacing(28)}>
 				{children}
 			</Box>
 		</>
